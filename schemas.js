2fe49ec601c140447476508d67aa5c79258617e3
Tejas <saitejas222@gmail.com>
2023-04-25 22:24:55 +0530

    lifted

---
+++ schemas.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const student = new mongoose.Schema({
+    // rollno:{
+    //     type:String,
+    //     required:true
+    // },
+    // firstName:{
+    //     type:String,
+    //     required:true
+    // }
+})
+
+const faculty = new mongoose.Schema({
+    
+})
