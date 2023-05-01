d4cd6d933a7ca32d0f4f1817fa15007bb3accd07
Tejas <saitejas222@gmail.com>
2023-05-01 23:46:44 +0530

    reloading, rerouting added

--- et-website/src/app/app.component.ts
+++ et-website/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'et-website';
+
+  getToken(){
+    if(localStorage.getItem("studentToken")){
+      return true
+    }
+    return false
+  }
 }
