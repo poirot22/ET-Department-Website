e8aa39602faf5b07cd9528fe2044b100bd0bd9a4
Tejas <saitejas222@gmail.com>
2023-06-25 16:47:16 +0530

    added devteam

---
+++ et-website/src/app/devteam/devteam.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-devteam',
+  templateUrl: './devteam.component.html',
+  styleUrls: ['./devteam.component.css']
+})
+export class DevteamComponent {
+
+}
