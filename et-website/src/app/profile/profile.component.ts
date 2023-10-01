c93fc5640aff877eaa9aa44cf54a2bcf911ba304
nikhil5022 <gsn.agc5022@gmail.com>
2023-10-01 21:25:46 +0530

    ok

---
+++ et-website/src/app/profile/profile.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent {
+    rollNo:any=""
+    user:any=""
+    posts:any=""
+    userPosts:any=[]
+    constructor( private router:ActivatedRoute,private http:HttpClient){  
+    }
+
+    async ngOnInit(){
+      this.rollNo=this.router.snapshot.paramMap.get('rollno');
+      const userDataResp = await this.http.get("http://localhost:9000/getStudentByRollNo/" + this.rollNo).toPromise();
+      this.user=userDataResp["Student Data"]
+      this.posts=this.user.posts
+      for(let i=0;i<this.posts.length;i++){
+        const postResp = await this.http.get("http://localhost:9000/getPostById/" + this.posts[i]).toPromise();
+        console.log(postResp)
+        this.userPosts.push(postResp["post"])
+      }
+      console.log(this.user)
+}
+}
