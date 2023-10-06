b645ffbe37a62d32fdd6551639a406c69ae22ae5
Tejas <saitejas222@gmail.com>
2023-10-06 10:54:21 +0530

    made minor changes

--- et-website/src/app/comment/comment.component.ts
+++ et-website/src/app/comment/comment.component.ts
@@ -13,7 +13,7 @@ import { HelperService } from '../helper.service';
 })
 export class CommentComponent {
 
-  constructor(private route:ActivatedRoute,private http:HttpClient,private helper:HelperService,private router:Router){}
+  constructor(private route:ActivatedRoute,private http:HttpClient,public helper:HelperService,private router:Router){}
 
   commentForm=new FormGroup({
     comment: new FormControl(),
@@ -26,25 +26,40 @@ export class CommentComponent {
   comments:any=[]
   commentIDs:any=[]
   userRoll=localStorage.getItem('rollno')
-  ngOnInit(){
+  async ngOnInit(){
+    /*function sleep (time) {
+      return new Promise((resolve) => setTimeout(resolve, time));
+    }
+    
+    await sleep(500).then(() => {
+      console.log("slept")
+    });*/
+
+
     this.postId = this.route.snapshot.paramMap.get('id');
-    this.http.get("http://localhost:9000/getPostById/"+this.postId).subscribe(resp=>{
+    /*this.http.get("http://localhost:9000/getPostById/"+this.postId).subscribe(resp=>{
         this.temp=resp
         this.post=this.temp.post
-        
-        /*this.http.get("http://localhost:9000/getCommentsOnPost/"+this.postId).subscribe(resp=>{
-          this.temp2=resp
-          this.comments=this.temp2.comments
-        })*/
-        this.commentIDs=this.post.comments
+      
+        this.commentIDs=this.post.comments.slice()
         for(let i=0;i<this.commentIDs.length;i++){
-          this.http.get("http://localhost:9000/getCommentById/"+this.commentIDs[i]).subscribe(resp=>{
-            //this.comments[i].postedBy=resp
-            this.temp2=resp
-            this.comments.push(this.temp2.comment)
-          })
+       
+
+          const resp= await this.http.get("http://localhost:9000/getCommentById/"+this.commentIDs[i]).toPromise()
+          //const postsResp = await this.http.get("http://localhost:9000/getAllPosts").toPromise();
+          this.temp2=resp
+          this.comments.push(this.temp2.comment)
         }
-    })
+    })*/
+    const resp = await this.http.get("http://localhost:9000/getPostById/"+this.postId).toPromise();
+    this.temp = resp;
+    this.post = this.temp.post;
+    this.commentIDs = this.post.comments.slice();
+    for(let i=0;i<this.commentIDs.length;i++){
+      const resp2 = await this.http.get("http://localhost:9000/getCommentById/"+this.commentIDs[i]).toPromise();
+      this.temp2 = resp2;
+      this.comments.push(this.temp2.comment);
+    }
   }
   postedComment:any=""
   commentBody:any=""
